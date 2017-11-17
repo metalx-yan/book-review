e6e77ec1531a3ff7f8977f94d0e8099ec72b1e11
Firadus <angga.firdaus@raharja.info>
2017-11-17 13:21:38 +0700

    register

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container" style="margin-top:50px;">
+    <div class="row">
+    	<div class="col-md-8">
+     	 	<span class="label label-default">Library</span>
+			<span class="label label-info">Reading</span>
+			<span class="label label-success">Book</span>
+			<span class="label label-primary">Dictionary</span>
+			<span class="label label-warning">Study</span><br>
+			<div class="span" ;">
+				<span class="label label-danger">Religion</span>
+				<span class="label label-danger">Programing</span>
+				<span class="label label-danger">Scientific</span>
+				<span class="label label-danger">World</span>
+			</div>
+			<h1 class="display-3">Hello <b class="buku">Buku</b> !! </h1>
+			<p class="lead">Jendela dunia berisi tumpukan ilmu yang kelak akan menjadikan Putra-Putri
+			Ibu pertiwi bebas menggapai dunia.</p>
+			<hr class="my-4">
+			<h2>Review<b class="buku"> Buku</b><br>Sebelum beli<br><b class="buku">Buku</b></span> nya !!</h2>
+		</div>
+   		<div class="col-md-4">
+     	 	<form>
+     	 		<div class="form-group">
+			    	<input type="text" class="form-control" placeholder="Nama Lengkap">
+			  	</div>
+			  	<div class="form-group">
+			   		<input type="email" class="form-control" placeholder="Email">
+			  	</div>
+			  	<div class="form-group">
+			    	<input type="password" class="form-control" placeholder="Password">
+			  	</div>
+			  	<div class="form-group">
+			    	<input type="password" class="form-control" placeholder="Password Confirmation">
+			  	</div>
+			  	<a href="#" class="btn btn-success">Register</a>
+			</form>
+    	</div>
+  	</div>       
+</div>
+@endsection
